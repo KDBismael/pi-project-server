7f26dc8fac1168ca14bcc7a7c90d22807bf9ce2c
kdb-payqin <ismael@payqin.com>
2024-03-01 17:02:45 +0000

    add user schema

---
+++ src/schema/user-schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
+
+export class User {
+  @Prop()
+  firstName: string;
+  @Prop()
+  lastName: string;
+  @Prop()
+  type: string;
+  @Prop()
+  title?: string;
+  @Prop({ lowercase: true, unique: true })
+  email: string;
+  @Prop()
+  password: string;
+}
+
+export type UserDocument = User & Document;
+export const UserSchema = SchemaFactory.createForClass(User);
